68a985a8061d6ebbafb35a61cca6932dbbd3d9d0
binny <90454079+binny3213@users.noreply.github.com>
2024-06-02 15:48:05 +0300

    Add files via upload
    Build a Updated Protfolio Website, using Javascript, Css3, Html5,  and Saas.

---
+++ app.js
@@ -0,0 +1,58 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0 ; i < sectBtn.length; i++)
+        {
+            sectBtn[i].addEventListener('click', function() {
+                let currentBtn = document.querySelectorAll('.active-btn');
+                currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+                this.className += ' active-btn';
+            })
+        }
+
+        allSections.forEach((section) =>{
+            section.addEventListener('click', (e)=> {
+                const id = e.target.dataset.id;
+    
+                if (id) {
+                    // Remove 'active' class from other buttons
+                    sectBtns.forEach((btn) => {
+                        btn.classList.remove('active');
+                    });
+            
+                    // Add 'active' class to the clicked button
+                    e.target.classList.add('active');
+            
+                    // Hide other sections
+                    sections.forEach((section) => {
+                        section.classList.remove('active');
+                    });
+            
+                    // Show the section with the corresponding ID
+                    const element = document.getElementById(id);
+                    element.classList.add('active');
+                }
+            });
+    
+
+        })
+     
+       
+      //Toggle Theme
+
+      const themeBtn = document.querySelector('.theme-btn');
+      themeBtn.addEventListener('click',()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode');
+      })
+
+        
+}
+
+PageTransitions();
+
